c99c99a0097ed4b4f3296e48c794cd87807278c5
Tom <tvdbon@gmail.com>
2021-04-25 16:14:41 +0200

    cleaned up - playable

--- game.py
+++ game.py
@@ -3,15 +3,25 @@ import math
 from pygame.locals import *
 import random
 
+import winsound
+from pygame import mixer
+
+mixer.pre_init(44100, -16, 2, 512)
+mixer.init()
+mixer.music.load('data/bensound-clearday.mp3')
+mixer.music.play(-1)
+#Credits to https://www.bensound.com/ for the music
+
+
 ''' Configurable variables '''
 WIDTH = 1024
 HEIGHT = 768
 
 characterMovementAmount = 2
-bgMovementAmount = characterMovementAmount + 4
+bgMovementAmount = characterMovementAmount 
 
 cX = int(WIDTH/2)
-cY = 50
+cY = 100
 fishSize = 20
 fishSpeed = 5
 hungerSpeed = 0.02
@@ -20,12 +30,42 @@ pygame.init()
 screen = pygame.display.set_mode((WIDTH,HEIGHT))
 clock = pygame.time.Clock()
 
-#bg = pygame.Surface((WIDTH, OCEANDEPTH))
-#bg.fill((124, 207, 255))
+# graphics
+startScreen = pygame.image.load("data/bg_menu.png").convert_alpha()
+loseScreen = pygame.image.load("data/lose.png").convert_alpha()
+winScreen = pygame.image.load("data/win.png").convert_alpha()
+heart = pygame.image.load("data/heart_icon.png").convert_alpha()
+seaweed = pygame.image.load("data/seaweed.png").convert_alpha()
 
-startScreen = pygame.image.load("menu.png").convert_alpha()
-loseScreen = pygame.image.load("lose.png").convert_alpha()
-winScreen = pygame.image.load("win.png").convert_alpha()
+# sounds effects all public domain
+eatsound = pygame.mixer.Sound("data/bloop_x.wav")
+damagesound = pygame.mixer.Sound("data/ouch.wav")
+losesound = pygame.mixer.Sound("data/buzzer_x.wav")
+winsound = pygame.mixer.Sound("data/applause2_x.wav")
+
+class Bubble(pygame.sprite.DirtySprite):
+    def __init__(self, pos):
+        super(Bubble, self).__init__()
+
+        self.image = pygame.image.load("data/bubble.png").convert_alpha()
+        self.rect = self.image.get_rect(center=pos)
+        self.x = pos[0]
+        self.y = pos[1]
+        self.wiggle = 1
+
+    def update(self, bgViewPort):
+        currentPos = (self.x, self.y)        
+
+        if currentPos[1] > 0:
+            newX = currentPos[0]
+
+            self.wiggle = not self.wiggle
+            newY = currentPos[1] - 1
+            self.rect = self.image.get_rect(center=(newX, newY - bgViewPort))
+            self.x = newX
+            self.y = newY
+        else:
+            self.kill()
 
 class Player(pygame.sprite.DirtySprite):
     
@@ -40,7 +80,7 @@ class Player(pygame.sprite.DirtySprite):
 
         self.image = pygame.Surface((80, 50), pygame.SRCALPHA)
         self.rect = self.image.get_rect(center=pos)
-        self.spritesheet = pygame.image.load("player.png").convert_alpha()
+        self.spritesheet = pygame.image.load("data/player.png").convert_alpha()
         self.blitImage(self.index)
 
         self.prevx = pos[0]
@@ -48,13 +88,13 @@ class Player(pygame.sprite.DirtySprite):
         self.mask = pygame.mask.from_surface(self.image)
         self.animSpeed = 2
         self.animSpeedCounter = self.animSpeed
-        self.eatingTimes = 2
+        self.eatingTimes = 1
         self.eatingCounter = self.eatingTimes
         self.skip = 1
         
     def damage(self, fish):
         enemyPos = fish.rect.center
-        playerPos = player.rect.center
+        playerPos = self.rect.center
 
         newX = playerPos[0]
         newY = playerPos[1]
@@ -70,11 +110,9 @@ class Player(pygame.sprite.DirtySprite):
 
     def grow(self):
         self.size += 1
-        print(self.size)
         self.blitImage(self.index)
 
     def eating(self):
-        print("eating")
         self.state = 3 
         self.index = 24
         self.blitImage(self.index)
@@ -82,7 +120,6 @@ class Player(pygame.sprite.DirtySprite):
         self.eatingCounter = self.eatingTimes
 
     def blitImage(self, index):        
-        #print("getfish")
         y = int(index / 8)
         x = index % 8
 
@@ -95,28 +132,20 @@ class Player(pygame.sprite.DirtySprite):
         self.mask = pygame.mask.from_surface(self.image)
 
         if self.direction == 0:
-            #print("flip")
             self.image = pygame.transform.flip(self.image, True, False)       
 
     def update(self):
-        print("update: " + str(self.state) + "," + str(self.index))
-        if self.state == 3:
-            print(self.index)
         if self.animSpeedCounter <= 0:
             self.index += 1
 
             if self.state == 1 and self.index > 23:
-                print("rest state")
                 self.index = 16
 
             if self.state == 3 and self.index > 27:
-                print("eat state")                
                 if self.eatingCounter < 0:
-                    print("reset eat")
                     self.state = 1
                     self.index = 16
                 else:
-                    print("continue eat")
                     self.index = 24
                     self.eatingCounter -= 1
 
@@ -160,6 +189,39 @@ class Player(pygame.sprite.DirtySprite):
     def getSize(self):
         return self.size
 
+class Seaweed(pygame.sprite.Sprite):
+    def __init__(self, pos):
+        super(Seaweed, self).__init__()
+
+        self.image = pygame.Surface((25, 128), pygame.SRCALPHA)
+
+        self.spritesheet = pygame.image.load("data/seaweed.png").convert_alpha()
+        self.rect = self.image.get_rect(center=pos)
+        self.x = pos[0]
+        self.y = pos[1]
+        self.animSpeed = 20
+        self.animSpeedCounter = self.animSpeed
+        self.index = 0
+        self.blitImage(self.index)
+
+    def blitImage(self, index):     
+        self.image.fill(0)   
+        self.image.blit(self.spritesheet, (0,0), (index * 25, 0, 25,128))
+
+    def update(self, bgViewPort):
+        if self.animSpeedCounter <= 0:
+            self.index += 1
+
+            if self.index > 3:
+                self.index = 0
+
+            self.blitImage(self.index)
+            self.animSpeedCounter = self.animSpeed
+
+        self.animSpeedCounter -= 1
+
+        self.rect = self.image.get_rect(center=(self.x,self.y - bgViewPort))
+
 
 class Fish(pygame.sprite.Sprite):
     
@@ -173,7 +235,7 @@ class Fish(pygame.sprite.Sprite):
         self.image = pygame.Surface((80, 50), pygame.SRCALPHA)
         self.rect = self.image.get_rect(center=pos)
         fishtype = random.randint(1, 7)
-        self.spritesheet = pygame.image.load("enemy" + str(fishtype)+ ".png").convert_alpha()
+        self.spritesheet = pygame.image.load("data/enemy" + str(fishtype)+ ".png").convert_alpha()
         self.blitImage(self.index)
 
         self.prevx = pos[0]
@@ -188,7 +250,6 @@ class Fish(pygame.sprite.Sprite):
         self.dieing = 10
         
     def blitImage(self, index):        
-        #print("getfish")
         y = int(index / 8)
         x = index % 8
 
@@ -200,11 +261,20 @@ class Fish(pygame.sprite.Sprite):
         self.mask = pygame.mask.from_surface(self.image)
 
         if self.direction == 1:
-            #print("flip")
             self.image = pygame.transform.flip(self.image, True, False)       
 
 
-    def update(self, bgViewPort):
+    def update(self, bgViewPort, spritelist):
+        if random.randint(0, 20) == 1:
+            rect = self.image.get_rect(center=(self.realPosX, self.realPosY))
+
+            if self.direction < 0:
+                newBubble = Bubble(rect.topleft)
+            else:
+                newBubble = Bubble(rect.topright)
+
+            spritelist.add(newBubble)
+
 
         if self.animSpeedCounter <= 0:
             self.index += 1
@@ -232,15 +302,12 @@ class Fish(pygame.sprite.Sprite):
        
         self.realPosX = newx
         self.realPosY = newy
-
         self.rect = self.image.get_rect(center=(newx,newy - bgViewPort))
-
-
    
     def getSize(self):
         return self.size
 
-bg = pygame.image.load("bg.png")
+bg = pygame.image.load("data/bg.png")
 OCEANWIDTH = bg.get_width()
 OCEANDEPTH = bg.get_height()
 
@@ -260,6 +327,7 @@ spawnTimer = 20
 player = Player((cX, cY))
 
 all_sprites = pygame.sprite.Group()
+bubble_list = pygame.sprite.Group()
 playersprite = pygame.sprite.Group()
 playersprite.add(player)
 
@@ -271,46 +339,78 @@ while loop:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             loop = 0
+            continue
 
-        if gameState == 0:
-            if event.type == pygame.KEYDOWN:
-                if event.key == K_SPACE:
-                    gameState = 1
-                    # counters
-                    bgViewPort = 0
-                    speedCounter = 0
-                    hungerMeter = 100
-                    health = 5
-                    pygame.key.set_repeat(10)
-                    spawnedFish = []
-                    spawnTimer = 20
-                    player = Player((cX, cY))
-                    all_sprites = pygame.sprite.Group()
-                    playersprite = pygame.sprite.Group()
-                    playersprite.add(player)
+        if event.type == pygame.KEYDOWN and gameState == 0:
+            if event.key == K_ESCAPE:
+                loop = 0
+                continue
 
-        if gameState == 1:
-            if event.type == pygame.KEYDOWN:
-                if event.key == K_DOWN:
-                    if cY > HEIGHT - 100 and bgViewPort < OCEANDEPTH - HEIGHT:
-                        bgViewPort += bgMovementAmount
-                        print(bgViewPort)
-                    else:
-                        if cY < HEIGHT - 20:
-                            cY += characterMovementAmount
-                if event.key == K_SPACE:
-                    player.grow()        
-                if event.key == K_UP:
-                    if cY < 100:
-                        if bgViewPort > 0:
-                            bgViewPort -= bgMovementAmount
-                    else:
-                        cY -= characterMovementAmount
+            if event.key == K_SPACE:
+                gameState = 1
+                # counters
+                bgViewPort = 0
+                speedCounter = 0
+                hungerMeter = 100
+                health = 5
+                pygame.key.set_repeat(10)
+                spawnedFish = []
+                
+                spawnTimer = 20
+                player = Player((cX, cY))
+                all_sprites = pygame.sprite.Group()
+                playersprite = pygame.sprite.Group()
+                bubble_list = pygame.sprite.Group()
+                playersprite.add(player)
 
-                if event.key == K_LEFT:
-                    cX -= characterMovementAmount
-                if event.key == K_RIGHT:
-                    cX += characterMovementAmount
+                newSeaweed = Seaweed((100,OCEANDEPTH-64))
+                bubble_list.add(newSeaweed)
+                newSeaweed = Seaweed((75,OCEANDEPTH-64))
+                bubble_list.add(newSeaweed)
+                newSeaweed = Seaweed((50,OCEANDEPTH-0))
+                bubble_list.add(newSeaweed)
+                newSeaweed = Seaweed((125,OCEANDEPTH-20))
+                bubble_list.add(newSeaweed)
+                newSeaweed = Seaweed((150,OCEANDEPTH-10))
+                bubble_list.add(newSeaweed)
+
+
+    if gameState == 0: # start screen
+        # spawn some fishies
+        spawnTimer -= 1
+        if spawnTimer <= 0:
+            spawnTimer = 120
+
+            direction = random.randint(0,1)
+            if direction == 1:
+                newFish = Fish((WIDTH + 50, random.randint(50, HEIGHT-50)), random.randint(0,4)*10, -1)
+            else:
+                newFish = Fish((-50, random.randint(50, HEIGHT-50)), random.randint(0,4)*10, 1)
+
+            all_sprites.add(newFish)
+
+    if gameState == 1: # playing game
+        keys_down = pygame.key.get_pressed()
+        if keys_down[K_DOWN]:
+            if cY > HEIGHT - 100 and bgViewPort < OCEANDEPTH - HEIGHT:
+                bgViewPort += bgMovementAmount
+                print(bgViewPort)
+            else:
+                if cY < HEIGHT - 20:
+                    cY += characterMovementAmount
+
+
+        if keys_down[K_UP]:
+            if cY < 100:
+                if bgViewPort > 0:
+                    bgViewPort -= bgMovementAmount
+            else:
+                cY -= characterMovementAmount
+
+        if keys_down[K_LEFT]:
+            cX -= characterMovementAmount
+        if keys_down[K_RIGHT]:
+            cX += characterMovementAmount
 
 
     if gameState == 1:
@@ -319,24 +419,23 @@ while loop:
             health -= 1
 
         if health <= 0:
+            pygame.mixer.Sound.play(losesound)
             gameState = 3
             gameEndCount = 0
 
         if player.size >= 50:
             gameState = 2
             gameEndCount = 0
+            pygame.mixer.Sound.play(winsound)
 
 
-        moveFish = False
+
         if speedCounter > fishSpeed:
-            moveFish = True
             speedCounter = 0
-            all_sprites.update(bgViewPort)
+            all_sprites.update(bgViewPort, bubble_list)
             playersprite.update()
 
-
         speedCounter += 1
-        moveFish = False
 
         UP, DOWN, LEFT, RIGHT = 0, 0, 0, 0
 
@@ -345,15 +444,13 @@ while loop:
         spawnTimer -= 1
 
         if spawnTimer <= 0:
-            print("spawn")
-            spawnTimer = 60
+            spawnTimer = 120
 
             direction = random.randint(0,1)
-            print(direction)
             if direction == 1:
                 newFish = Fish((WIDTH + 50, random.randint(100, OCEANDEPTH)), random.randint(0,4)*10, -1)
             else:
-                newFish = Fish((-50, random.randint(1, OCEANDEPTH-100)), random.randint(0,4)*10, 1)
+                newFish = Fish((-50, random.randint(100, OCEANDEPTH-100)), random.randint(0,4)*10, 1)
 
             all_sprites.add(newFish)
 
@@ -365,21 +462,24 @@ while loop:
                 print("Collision: " + str(fish.getSize()) + str(player.getSize()))
                 
                 if player.getSize() >= fish.getSize():
+                    player.eating()
+                    pygame.mixer.Sound.play(eatsound)
                     hungerMeter += 5
                     player.grow()
                     all_sprites.remove(fish)
-                    player.eating()
                 else:
+                    pygame.mixer.Sound.play(damagesound)
                     health -= 1
                     (cX, cY) = player.damage(fish)
 
-
         player.updatePlayer((cX, cY))
+        bubble_list.update(bgViewPort)
 
         # render screen
         screen.fill((0,0,0))
         screen.blit(bg, (0, 0), area = (0, bgViewPort,WIDTH,HEIGHT))
 
+        bubble_list.draw(screen)
         all_sprites.draw(screen)
         playersprite.draw(screen)
 
@@ -393,11 +493,23 @@ while loop:
 
         # health
         for x in range(health):
-            pygame.draw.circle(screen, (250, 0, 0), (WIDTH - (5 * 20) + (x * 20),20), 10)
+            #pygame.draw.circle(screen, (250, 0, 0), (WIDTH - (5 * 20) + (x * 20),20), 10)
+            screen.blit(heart, (WIDTH - 20 - (5 * 20) + (x * 20),20) )
 
 
     if gameState == 0: # menu
         screen.blit(startScreen, (0,0))
+
+        if speedCounter > fishSpeed:
+            speedCounter = 0
+            all_sprites.update(0, bubble_list)
+
+        speedCounter += 1
+
+        bubble_list.update(0)
+
+        bubble_list.draw(screen)
+        all_sprites.draw(screen)
 
     if gameState == 2: # end of game - Win
         screen.blit(winScreen, (0,0))
@@ -407,9 +519,24 @@ while loop:
 
     if gameState == 2 or gameState == 3:
         gameEndCount += 1
-        if gameEndCount > 60:
+        if gameEndCount > 120:
             gameState = 0
-    
+
+            all_sprites = pygame.sprite.Group()
+            playersprite = pygame.sprite.Group()
+            bubble_list = pygame.sprite.Group()
+
+            newSeaweed = Seaweed((100,HEIGHT-64))
+            bubble_list.add(newSeaweed)
+            newSeaweed = Seaweed((75,HEIGHT-64))
+            bubble_list.add(newSeaweed)
+            newSeaweed = Seaweed((50,HEIGHT-0))
+            bubble_list.add(newSeaweed)
+            newSeaweed = Seaweed((125,HEIGHT-20))
+            bubble_list.add(newSeaweed)
+            newSeaweed = Seaweed((150,HEIGHT-10))
+            bubble_list.add(newSeaweed)
+
 
     clock.tick(60)
     pygame.display.update()
